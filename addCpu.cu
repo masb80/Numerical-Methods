13d7dee0c967ecbf693d9fad5be1544d6fb854fb
Samad Bhuiyan <masb80@users.noreply.github.com>
2018-02-09 09:51:37 -0330

    Update addCpu.cu

--- addCpu.cu
+++ addCpu.cu
@@ -1,3 +1,4 @@
+// Calculationg Two arrays in CPU usig CUDA compiler
 #include<studio.h>
 #define SIZE 1024
 
@@ -8,7 +9,7 @@ for(i=0; i < n; i++)
 c[i] = a[i] + b[i];
 
 int main(){
-int *a, *b, *c;
+int *a, *b, *c; // Host side pointer
 
 a = (int*)malloc(SIZE*sizeof(int));
 b = (int*)malloc(SIZE*sizeof(int));
@@ -22,7 +23,7 @@ c[i] = 0;
 
 vectorAdd(a, b, c, SIZE);
 
-for(int i = 0; i < 10; ++i)
+for(int i = 0; i < 10; ++i) // printing 1st 10 elements
 printf("c[%d] = %d\n", i, c[i]);
 
 free(a);
@@ -31,4 +32,4 @@ free(c);
 
 return 0;
 
-}
+} // main
