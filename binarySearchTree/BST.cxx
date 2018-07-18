15ed52522b54f3b78262eec4f3e82343b5aafb6c
masb80 <masb80@mun.ca>
2018-07-18 10:52:14 -0230

    updated binary search tree

--- binarySearchTree/BST.cxx
+++ binarySearchTree/BST.cxx
@@ -5,7 +5,7 @@ using namespace std;
 
 	BST::BST()
 	{
-		root = NULL;
+		root = NULL; // Constructor for making root node empty initially
 	}
 
 
@@ -15,33 +15,32 @@ using namespace std;
 		n->key = key; // initialize key
 		n->left = NULL; // initialize left as NULL
 		n->right = NULL; // initialize right as NULL
-
 		return n;
-
 	}
 	void BST::addLeaf(int key)
 	{
-		addLeafPrivate(key, root);
-		
+		addLeafPrivate(key, root);		
 	}
 	void BST::addLeafPrivate(int key, Node *Ptr)
 	{
 		if(root == NULL)
 			{ 
-				root = createLeaf(key);
+				root = createLeaf(key); // making leaf by key
+				cout << "In root node,  key =  " << key << ",  root =   " << root->key<<  endl;	 
 			}
-		else if(key < Ptr->key)
+		else if(key < Ptr->key) // given value is less than present root
 			{
-				 if(Ptr->left != NULL)
+				 if(Ptr->left != NULL)// child left node is not empty
 				 {
-				 	 addLeafPrivate(key, Ptr->left);
+				 	 addLeafPrivate(key, Ptr->left); // recursion of chack of root again for left node
 				 }
 				 else
 				 {
-				 	Ptr->left = createLeaf(key);
+				 	Ptr->left = createLeaf(key); // if empty then add and make leaf node with that
 				 }
+				cout << "In left key,  key =  " << key << ",     Ptr->left->key = " << Ptr->left->key <<  endl;	 
 			}
-		else if(key > Ptr->key)
+		else if(key > Ptr->key) //  similarly for right node
 			{
 				if(Ptr->right != NULL)
 				{
@@ -51,38 +50,36 @@ using namespace std;
 				{
 					Ptr->right = createLeaf(key);
 				}
+				cout << "In rightt key,  key =  " << key << ",  Ptr->right->key =  " << Ptr->right->key <<  endl;	 
 			}
 		else
 			{
 				cout << "The given key  " << key << "has already been added" << endl;
 			}
-
 	}
 
-void BST::printInOrder()
-{
-	printInOrderPrivate(root);	
-}
 void BST::printInOrderPrivate(Node * Ptr)
 {
-	if(root != NULL)
+	if(root != NULL) // if root node is not empty
 	{
-		if(Ptr->left != NULL)
+		if(Ptr->left != NULL) // if left node is not empty  : step 1: go left
 		{
-			printInOrderPrivate(Ptr->left);
+			printInOrderPrivate(Ptr->left); // recursion like root node
 		}
-			cout << Ptr->left << endl;
-		if(Ptr->right != NULL)
+			cout << Ptr->key << "  "; // if empty then print it: step 2
+		if(Ptr->right != NULL) // if right node is not empty : step 3: go right
 		{
-			printInOrderPrivate(Ptr->right);
+			printInOrderPrivate(Ptr->right); // recursion like root node
 
 		}
-		//cout << Ptr->right << endl;
 	}
-	else
+	else // if root node is empty then the tree is empty
 	{
-		cout << "The tree is empty" << endl;
+		cout << "The tree is empty\n" << endl;
 	}
-
+}
+void BST::printInOrder()
+{
+	printInOrderPrivate(root);	// making print private for giving root node
 }
 
--- binarySearchTree/main.cxx
+++ binarySearchTree/main.cxx
@@ -5,17 +5,15 @@ using namespace std;
 
 int main()
 {
-	int arr[5] = {4,2,56,3,6};
+	int arr[] = {4,2,56,3,6,5,6,34,35,78,12,13,15,17,23,45,56,78,90,100};
 	BST bst;
-
-	for(int i = 0; i < 5; i++)
-	{
-		bst.addLeaf(arr[i]);
-	}
-	
 	bst.printInOrder();
-
-	cout << "compilation successful" << endl;
+	for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+	{
+		bst.addLeaf(arr[i]); // creating leaf with the given aray
+	}	
+	bst.printInOrder();
+	cout << "\ncompilation successful" << endl;
 	return 0;
 }
 
